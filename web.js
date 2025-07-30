a635e0c68a0f872a1acf55dca57f1c9c263a3957
tharunnaidu87-lab <tharunnaidu87@gmail.com>
2025-07-30 22:41:58 +0530

    first commit

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
 document.getElementById('helloBtn').addEventListener('click', () => {
-    alert('Hello! Thanks for clicking the button. Visit Again!, Thank You!');
+    alert('Hello! Thanks for clicking the button.');
 });
+
